698f781d2bb86c57505f3d778ac7b6524a55c127
nathanli <nathaniel.li@utexas.edu>
2022-09-28 19:35:24 -0500

    changed to for and added 1-100

--- basic2.c
+++ basic2.c
@@ -4,6 +4,17 @@
 #include <time.h>
 
 
+
+int sum_one_to_hundred()
+{
+    int sum = 0;
+    for(int i = 1; i <= 100; ++i)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
 int main(int argc, char** argv)
 {
     int month = 9; 
@@ -11,8 +22,9 @@ int main(int argc, char** argv)
     printf("%d/%d\n", day, month);
 
     srand(time(NULL));
-    int target = rand() % 100;
     int num = 0;
+    /*
+    int target = rand() % 100;
     while(true)
     {
 
@@ -31,5 +43,27 @@ int main(int argc, char** argv)
         }
         
     }
+    //*/
+    printf("input num:");
+    scanf("%d", &num);
+    for(int target = rand() % 100; num != target; scanf("%d", &num))
+    {
+        if(num > target)
+        {
+            printf("greater\n");
+        } else if (num < target)
+        {
+            printf("less\n");
+        } else 
+        {
+            printf("equal\n");
+            break;
+        }
+        printf("input num:");
+    }
+    // 1. change while to for
+    // 2. write a function: add 1 to 100 
+    printf("%d\n", sum_one_to_hundred());
+
     return 0;
 }
